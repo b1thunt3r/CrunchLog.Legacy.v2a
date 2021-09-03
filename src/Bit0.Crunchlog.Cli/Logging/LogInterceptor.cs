526b7321607da9e22bb50f82c6a05bc5be13e977
b1thunt3r <ishan@jain.se>
2021-09-03 23:59:09 +0100

    Override initial log level

--- src/Bit0.Crunchlog.Cli/Logging/LogInterceptor.cs
+++ src/Bit0.Crunchlog.Cli/Logging/LogInterceptor.cs
@@ -1,12 +1,13 @@
 ﻿using Bit0.CrunchLog.Cli.Commands.Settings;
 using Serilog.Core;
+using Serilog.Events;
 using Spectre.Console.Cli;
 
 namespace Bit0.CrunchLog.Cli.Logging
 {
     internal class LogInterceptor : ICommandInterceptor
     {
-        public static readonly LoggingLevelSwitch LogLevel = new();
+        public static readonly LoggingLevelSwitch LogLevel = new(LogEventLevel.Error);
 
         public void Intercept(CommandContext context, CommandSettings settings)
         {
