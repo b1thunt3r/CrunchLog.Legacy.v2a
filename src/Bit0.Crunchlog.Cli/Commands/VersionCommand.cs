21edf231387faa15fd77c65ccfe39a7998151f00
b1thunt3r <ishan@jain.se>
2021-09-11 20:13:33 +0100

    Add OS info to version

--- src/Bit0.Crunchlog.Cli/Commands/VersionCommand.cs
+++ src/Bit0.Crunchlog.Cli/Commands/VersionCommand.cs
@@ -3,13 +3,14 @@ using Spectre.Console;
 using Spectre.Console.Cli;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Bit0.CrunchLog.Cli.Commands
 {
     internal sealed class VersionCommand : Command<EmptyCommandSettings>
     {
-        public override Int32 Execute([NotNull] CommandContext context, [NotNull] EmptyCommandSettings settings)
+        public override int Execute([NotNull] CommandContext context, [NotNull] EmptyCommandSettings settings)
         {
             AnsiConsole.Markup("[green]CrunchLog[/]");
 #if DEBUG
@@ -23,18 +24,29 @@ namespace Bit0.CrunchLog.Cli.Commands
             var grid = new Grid();
             grid.AddColumn(new GridColumn().NoWrap());
             grid.AddColumn(new GridColumn());
-            grid.AddRow("[yellow]Core Libraries[/]");
-            grid.AddRow($"Bit0.CrunchLog", $"{FileVersionInfo.GetVersionInfo(typeof(CrunchLog).Assembly.Location).ProductVersion}");
-            grid.AddRow($"Bit0.CrunchLog.Sdk", $"{FileVersionInfo.GetVersionInfo(typeof(CrunchConfig).Assembly.Location).ProductVersion}");
-            grid.AddRow($"Bit0.CrunchLog.Cli", $"{FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).ProductVersion}");
+            grid.AddRow("[wheat1]Core Libraries[/]");
+            grid.AddRow($"Bit0.CrunchLog", $"{GetProductVersion(typeof(CrunchLog))} ({GetTargetFramework(typeof(CrunchLog))})");
+            grid.AddRow($"Bit0.CrunchLog.Sdk", $"{GetProductVersion(typeof(CrunchConfig))} ({GetTargetFramework(typeof(CrunchConfig))})");
+            grid.AddRow($"Bit0.CrunchLog.Cli", $"{GetProductVersion(typeof(Program))} ({GetTargetFramework(typeof(Program))})");
             grid.AddRow("");
-            grid.AddRow("[yellow]Environment[/]");
+            grid.AddRow("[wheat1]Environment[/]");
+            grid.AddRow($"Operating System", RuntimeInformation.OSDescription);
             grid.AddRow($".NET", RuntimeInformation.FrameworkDescription.Replace(".NET ", ""));
             grid.AddRow($"RID", RuntimeInformation.RuntimeIdentifier);
             grid.AddRow($"[bold]Working Directory[/]", Environment.CurrentDirectory);
             AnsiConsole.Render(grid);
 
             return 0;
+        }
+
+        private static string GetTargetFramework(Type type)
+        {
+            return type.Assembly.GetCustomAttribute<System.Runtime.Versioning.TargetFrameworkAttribute>()?.FrameworkName ?? "Unknown";
+        }
+
+        private static string GetProductVersion(Type type)
+        {
+            return FileVersionInfo.GetVersionInfo(type.Assembly.Location).ProductVersion;
         }
     }
 }
